2782edcb01df0597e4590fd0960716713c6fa3ff
muhammedyildiz3465@gmail.com <63956100+muhammedyildizbas@users.noreply.github.com>
2021-03-28 00:15:02 +0300

    Main Activity 'nin Register ve Login Activity ile bağlantısı

--- app/src/main/java/com/example/hellochat/MainActivity.java
+++ app/src/main/java/com/example/hellochat/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.hellochat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +21,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
        init();
+
+       btnWelcomeLogin.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intentLogin = new Intent(MainActivity.this, LoginActivity.class);
+               startActivity(intentLogin);
+           }
+       });
+
+        btnWelcomeRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentRegister = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intentRegister);
+            }
+        });
 
     }
 }
