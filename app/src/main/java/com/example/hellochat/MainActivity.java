6ac793db3ac9880ca2f560be118e49022b6fcf17
muhammedyildiz3465@gmail.com <63956100+muhammedyildizbas@users.noreply.github.com>
2021-03-27 23:55:47 +0300

    Ana Ekran butonları kodları

--- app/src/main/java/com/example/hellochat/MainActivity.java
+++ app/src/main/java/com/example/hellochat/MainActivity.java
@@ -3,13 +3,22 @@ package com.example.hellochat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+        private Button btnWelcomeLogin,btnWelcomeRegister;
+
+        public  void  init(){
+            btnWelcomeLogin = (Button) findViewById(R.id.btnWelcomeLogin);
+            btnWelcomeRegister = (Button) findViewById(R.id.btnWelcomeRegister);
+
+        }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("ana ekran olsuturuldu kod yazılmalı.");
+       init();
+
     }
 }
