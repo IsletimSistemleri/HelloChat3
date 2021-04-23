e05c18091ab2bfb48a6c869946261361e3f532ed
zeynepcagla <75018194+zeynepcagla@users.noreply.github.com>
2021-04-23 06:39:58 +0300

    firebaseyleekranlarınaktiflestirilmesi

--- app/src/main/java/com/example/hellochat/LoginActivity.java
+++ app/src/main/java/com/example/hellochat/LoginActivity.java
@@ -1,23 +1,85 @@
 package com.example.hellochat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-    private Toolbar actionbarLogin;
-    public  void init(){
-        actionbarLogin=findViewById(R.id.action_bar_Login);
-        setSupportActionBar(actionbarLogin);
-        getSupportActionBar().setTitle("HESABIM VAR");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
+   Context context=this;
+    EditText editTextUsername,editTextPassword;
+
+    FirebaseAuth mAuth;
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        init();
+        mAuth=FirebaseAuth.getInstance();
+        editTextUsername=findViewById(R.id.editTextUsername);
+        editTextPassword=findViewById(R.id.editTextPassword);
     }
+    public  void login(View view){
+        String username=editTextUsername.getText().toString().trim();
+        String password=editTextPassword.getText().toString().trim();
+
+        if(username.isEmpty()){
+            Toast.makeText(context,"Kullanıcı Adı Alanı Boş Geçilemez " , Toast.LENGTH_LONG).show();
+
+            editTextUsername.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            Toast.makeText(context,"Şifre Alanı Boş Geçilemez " , Toast.LENGTH_LONG).show();
+            editTextPassword.requestFocus();
+            return;
+    }
+
+        mAuth.signInWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                //geriye task değeri dönecek
+
+                if(task.isSuccessful()){
+Intent intent=new Intent(context,MessageActivity.class);
+startActivity(intent);
+finish();
+
+                }else{
+
+                    Toast.makeText(context,"Kullanıcı adı veya şifre hatalı  " , Toast.LENGTH_LONG).show();
+                    //şifre kutuucuğunun içini boşalt
+                    editTextPassword.setText("");
+                    //git şifre kutucuğuna konumlan
+                    editTextPassword.requestFocus();
+                }
+            }
+        });
+
+
+
+
+
+
+
+}
+public  void  register(View view){
+    Intent intent=new Intent(context,MainActivity.class);
+    startActivity(intent);
+    finish();
+}
+
 }
